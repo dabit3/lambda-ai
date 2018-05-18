9358a1347c7a4c3c791867e07ca9c57becc63fb5
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2018-05-18 06:30:57 -0700

    updated voice to match language

--- index.js
+++ index.js
@@ -23,17 +23,27 @@ exports.handler = (event, context, callback) => {
     if (err) callback(err)
     message = data.TranslatedText
 
+    const voices = {
+      'es': 'Penelope',
+      'pt': 'Vitoria',
+      'de': 'Vicki',
+      'en': 'Joanna',
+      'fr': 'Celine'
+    }
+
+    const voice = voices[event.code]
+
     var pollyParams = {
       OutputFormat: "mp3", 
       SampleRate: "8000", 
       Text: message,
       TextType: "text", 
-      VoiceId: "Joanna"
+      VoiceId: voice
     };
 
+    // step 2: synthesize the translation into speech  
     polly.synthesizeSpeech(pollyParams, function(err, data) {
       if (err) console.log(err, err.stack); // an error occurred
-      
       else  {
         let key = uuidV4()
         var params2 = {
